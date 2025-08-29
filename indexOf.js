3a17c49272d093c387a32431ca1c2c705766a48e
Nur Alam <nuralam1122345@gmail.com>
2025-08-29 15:25:00 +0600

    indexOf

---
+++ indexOf.js
@@ -0,0 +1,5 @@
+
+const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'fabul', 'gabul', 'habul', 'ibul', 'jabul'];
+console.log(names.indexOf('gabul'));
+
+// for show result [node indexOf.js]
