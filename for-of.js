756107e00f63f004ea72f7e3bbe3a115db5a6b9c
Nur Alam <nuralam1122345@gmail.com>
2025-08-30 21:38:23 +0600

    for-of

---
+++ for-of.js
@@ -0,0 +1,16 @@
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+for (const number of numbers) {
+    console.log(number);
+}
+
+const fruits = ['apple', 'banana', 'orange', 'mango', 'grape'];
+console.log(fruits);
+
+for (const fruit of fruits) {
+    console.log(fruit)
+    console.log('i want to eat')
+    console.log('its good')
+}
