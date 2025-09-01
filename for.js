51d211e6a8c2003d825f0f556ca1be870ddd9331
Nur Alam <nuralam1122345@gmail.com>
2025-09-01 13:06:13 +0600

    sum

--- for.js
+++ for.js
@@ -1,9 +1,32 @@
 
-for(let num = 0; num < 5; num++) {
-    console.log(num);
+// for(let num = 0; num < 5; num++) {
+//     console.log(num);
     
+// }
+
+// for(var i = 0; i <= 60; i++) {
+//     console.log('hello');
+//     }
+
+// for (let i = 1; i <= 30; i++){
+//     if(i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 1; i <= 30; i++){
+//     if(i % 3===0 || i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+let total = 0;
+for (let i = 0; i <= 50; i++){
+    if(i % 3===0){
+        console.log(i);
+        total = total + i;
+        console.log('total:', total);
+    }
 }
 
-for(var i = 0; i <= 60; i++) {
-    console.log('hello');
-    }
+console.log('total:', total);
