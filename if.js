b3db71957a3d858a84b5001e8c7ab6ac7ecbec29
Nur Alam <nuralam1122345@gmail.com>
2025-09-03 22:05:49 +0600

    add

--- if.js
+++ if.js
@@ -1,14 +1,23 @@
 
 
 
-var weight = 40;
+// var weight = 40;
 
-if (weight < 20) {
-    console.log('i will carry it by myself')
-}
-else {
-    console.log('i will call a rickshaw')
+// if (weight < 20) {
+//     console.log('i will carry it by myself')
+// }
+// else {
+//     console.log('i will call a rickshaw')
+// }
+
+// const number = [1, 2, 3, 4, 5]
+// console.log(number.length);
+
+const number = 80;
+
+if (number >= 80) {
+    console.log('you got gpa-5');
+} else {
+    console.log('you fail');
 }
 
-const number = [1, 2, 3, 4, 5]
-console.log(number.length);
