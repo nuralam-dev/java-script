a63ce427bc2a265e26bb117bf5ba7b34c36cdb51
Nur Alam <nuralam1122345@gmail.com>
2025-09-08 16:05:53 +0600

    if else

--- conceptual/operator.js
+++ conceptual/operator.js
@@ -10,3 +10,14 @@ var x = 40;
 x += 2
 console.log(x)
 
+var num = 290.565;
+console.log(parseInt(num));
+
+// if else { ! its mean not equal}
+    var num = 3;
+
+if (num == 3){
+    console.log('give me a pice');}
+else{
+    console.log('give me two pice');
+}
