7642c97d0e0e4fc2c8093970852528cf3bb176ff
Nur Alam <nuralam1122345@gmail.com>
2025-09-08 13:07:14 +0600

    js

---
+++ conceptual/operator.js
@@ -0,0 +1,13 @@
+var x = 8;
+var y = 9;
+var total = x + y - 8;
+console.log('total', total)
+
+var modulus = 23;
+console.log('modulus', modulus % 2);
+
+var x = 40;
+x += 2
+console.log(x)
+
+var y = 
